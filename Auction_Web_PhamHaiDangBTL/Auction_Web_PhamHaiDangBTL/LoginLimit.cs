cc699844e733c246eb74687fc56c795b3c73562f
khanhhoa790 <68863404+khanhhoa790@users.noreply.github.com>
2021-11-25 00:37:08 +0700

    autionweb training

---
+++ Auction_Web_PhamHaiDangBTL/Auction_Web_PhamHaiDangBTL/LoginLimit.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ConsultantSystem
+{
+    public class LoginLimit
+    {
+        public int failCount;
+        public DateTime timeLimit;
+    }
+}
